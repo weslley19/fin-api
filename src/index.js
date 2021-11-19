d2fe7f95bb1619dbf1179c716c560bcc7456b60d
Weslley Oliveira <weslleyoliveira258@gmail.com>
2021-11-19 20:48:52 -0300

    feat: show statement

--- src/index.js
+++ src/index.js
@@ -9,17 +9,17 @@ app.use(express.json())
 const costumers = [];
 
 function verifyExistsCostumerCpf(req, res, next) {
-  const { cpf } = req.body;
+  const { cpf } = req.headers;
 
-  const costumer = costumers.find(costumer => costumer.cpf === cpf)
+  const costumer = costumers.find((costumer) => costumer.cpf == cpf)
 
   if (!costumer) {
     return res.status(400).send({message: 'Costumer not found'});
   }
 
-  request.costumer = costumer;
+  req.costumer = costumer;
 
-  next()
+  return next()
 }
 
 app.get('/ping', (req, res) => {
@@ -45,6 +45,11 @@ app.post('/account', (req, res) => {
   costumers.push(costumer)
 
   return res.status(201).send({message: 'Account created successfully', data: costumers})
+})
+
+app.get('/statement', verifyExistsCostumerCpf, (req, res) => {
+  const { costumer } = req;
+  return res.status(200).send({message: 'Costumer found', statement: costumer.statement})
 })
 
 app.listen(PORT)
