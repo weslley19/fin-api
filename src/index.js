d0a59081d5d71402bb5738c889cca941453ec3f4
Weslley Oliveira <weslleyoliveira258@gmail.com>
2021-11-19 14:22:48 -0300

    feat: create middleware for account exists and create account

--- src/index.js
+++ src/index.js
@@ -1,16 +1,50 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid')
 
 const PORT = 3333
 
 const app = express();
 app.use(express.json())
 
+const costumers = [];
+
+function verifyExistsCostumerCpf(req, res, next) {
+  const { cpf } = req.body;
+
+  const costumer = costumers.find(costumer => costumer.cpf === cpf)
+
+  if (!costumer) {
+    return res.status(400).send({message: 'Costumer not found'});
+  }
+
+  request.costumer = costumer;
+
+  next()
+}
+
 app.get('/ping', (req, res) => {
     return res.send({message: 'pong'})
 })
 
 app.post('/account', (req, res) => {
+  const { name, cpf } = req.body;
 
+  const costumer = {
+    name,
+    cpf,
+    id: uuidv4(),
+    statement: []
+  }
+
+  const verifyExistsAccount = costumers.some(costumer => costumer.cpf === cpf)
+
+  if (verifyExistsAccount) {
+    return res.status(400).send({message: 'Account already exists'})
+  }
+
+  costumers.push(costumer)
+
+  return res.status(201).send({message: 'Account created successfully', data: costumers})
 })
 
 app.listen(PORT)
