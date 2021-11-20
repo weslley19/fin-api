1525497a3f798f7d26d52779d225740c615c55ae
Weslley Oliveira <weslleyoliveira258@gmail.com>
2021-11-19 22:49:55 -0300

    feta: filter for date

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function verifyExistsCostumerCpf(req, res, next) {
   const costumer = costumers.find((costumer) => costumer.cpf == cpf)
 
   if (!costumer) {
-    return res.status(400).send({message: 'Costumer not found'});
+    return res.status(400).send({ message: 'Costumer not found' });
   }
 
   req.costumer = costumer;
@@ -31,7 +31,7 @@ function getBalance(costumer) {
 }
 
 app.get('/ping', (req, res) => {
-    return res.send({message: 'pong'})
+  return res.send({ message: 'pong' })
 })
 
 app.post('/account', (req, res) => {
@@ -47,22 +47,38 @@ app.post('/account', (req, res) => {
   const verifyExistsAccount = costumers.some(costumer => costumer.cpf === cpf)
 
   if (verifyExistsAccount) {
-    return res.status(400).send({message: 'Account already exists'})
+    return res.status(400).send({ message: 'Account already exists' })
   }
 
   costumers.push(costumer)
 
-  return res.status(201).send({message: 'Account created successfully', data: costumers})
+  return res.status(201).send({ message: 'Account created successfully', data: costumers })
 })
 
 app.get('/costumer', verifyExistsCostumerCpf, (req, res) => {
   const { costumer } = req;
-  return res.status(200).send({message: 'Costumer found', data: costumer})
+  return res.status(200).send({ message: 'Costumer found', data: costumer })
 })
 
 app.get('/statement', verifyExistsCostumerCpf, (req, res) => {
   const { costumer } = req;
-  return res.status(200).send({message: 'Costumer found', statement: costumer.statement})
+  return res.status(200).send({ message: 'Costumer found', statement: costumer.statement })
+})
+
+app.get('/statement/date', verifyExistsCostumerCpf, (req, res) => {
+  const { costumer } = req;
+  const { date } = req.query
+
+  const dateFormat = new Date(date + ' 00:00')
+
+  const statement = costumer.statement.filter(costumer => costumer.createdAt.toDateString() == dateFormat.toDateString())
+  console.log(statement)
+
+  if (statement.length > 0) {
+    return res.status(200).json({ message: 'Costumer found', data: statement })
+  }
+
+  return res.status(404).json({ message: 'Costumer not found'})
 })
 
 // deposit | withdraw
@@ -81,13 +97,13 @@ app.post('/movement', verifyExistsCostumerCpf, (req, res) => {
 
       costumer.statement.push(statementOperation)
 
-      return res.status(201).send({message: 'Deposit accomplished successfully', data: costumer})
+      return res.status(201).send({ message: 'Deposit accomplished successfully', data: costumer })
     }
   } else {
     const balance = getBalance(costumer)
 
     if (balance < amount) {
-      return res.status(400).send({message: 'Insufficient balance'})
+      return res.status(400).send({ message: 'Insufficient balance' })
     }
 
     const statementOperation = {
@@ -98,10 +114,10 @@ app.post('/movement', verifyExistsCostumerCpf, (req, res) => {
 
     costumer.statement.push(statementOperation)
 
-    return res.status(200).send({message: 'Successful withdrawal', data: costumer})
+    return res.status(200).send({ message: 'Successful withdrawal', data: costumer })
   }
 
-  return res.status(400).send({message: 'Error when depositing'})
+  return res.status(400).send({ message: 'Error when depositing' })
 })
 
 app.put('/account', verifyExistsCostumerCpf, (req, res) => {
@@ -110,7 +126,7 @@ app.put('/account', verifyExistsCostumerCpf, (req, res) => {
 
   costumer.name = name
 
-  return res.status(200).send({message: 'Costumer updated successfully', data: costumer})
+  return res.status(200).send({ message: 'Costumer updated successfully', data: costumer })
 })
 
 app.listen(PORT)
