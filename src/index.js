c2dea460a5b3fb5e19582f244db778baa746c406
Weslley Oliveira <weslleyoliveira258@gmail.com>
2021-11-19 21:46:09 -0300

    feat: create operation withdraw

--- src/index.js
+++ src/index.js
@@ -22,6 +22,14 @@ function verifyExistsCostumerCpf(req, res, next) {
   return next()
 }
 
+function getBalance(costumer) {
+  const balance = costumer.statement.reduce((acc, item) => {
+    return acc += item.amount
+  }, 0)
+
+  return balance;
+}
+
 app.get('/ping', (req, res) => {
     return res.send({message: 'pong'})
 })
@@ -50,6 +58,45 @@ app.post('/account', (req, res) => {
 app.get('/statement', verifyExistsCostumerCpf, (req, res) => {
   const { costumer } = req;
   return res.status(200).send({message: 'Costumer found', statement: costumer.statement})
+})
+
+// deposit | withdraw
+app.post('/movement', verifyExistsCostumerCpf, (req, res) => {
+  const { costumer } = req;
+  const { amount, type, description } = req.body
+
+  if (type === 'deposit') {
+    if (amount > 0) {
+      const statementOperation = {
+        amount,
+        type,
+        description,
+        createdAt: new Date()
+      }
+
+      costumer.statement.push(statementOperation)
+
+      return res.status(201).send({message: 'Deposit accomplished successfully', data: costumer})
+    }
+  } else {
+    const balance = getBalance(costumer)
+
+    if (balance < amount) {
+      return res.status(400).send({message: 'Insufficient balance'})
+    }
+
+    const statementOperation = {
+      amount,
+      type,
+      createdAt: new Date()
+    }
+
+    costumer.statement.push(statementOperation)
+
+    return res.status(200).send({message: 'Successful withdrawal', data: costumer})
+  }
+
+  return res.status(400).send({message: 'Error when depositing'})
 })
 
 app.listen(PORT)
